79c99f184430b04325ab533e044cfb5cbcc277b6
Phumrapee Limpianchop <git@rayriffy.com>
2022-03-15 09:42:32 +0900

    chore: staged changes

--- src/constants/ongekiDirectory.ts
+++ src/constants/ongekiDirectory.ts
@@ -8,13 +8,17 @@ export const ongekiDirectory = {
   assets: path.join(basePath, '../Assets/Texture2D'),
   options: [
     path.join(basePath, 'package/mu3_Data/StreamingAssets/GameData/A000'),
-    ...fs
-      .readdirSync(path.join(basePath, 'package/option'))
-      .filter(
-        o =>
-          !o.startsWith('.') &&
-          fs.statSync(path.join(basePath, 'package/option', o)).isDirectory()
-      )
-      .map(o => path.join(basePath, 'package/option', o)),
+    ...(fs.existsSync(path.join(basePath, 'package/option'))
+      ? fs
+          .readdirSync(path.join(basePath, 'package/option'))
+          .filter(
+            o =>
+              !o.startsWith('.') &&
+              fs
+                .statSync(path.join(basePath, 'package/option', o))
+                .isDirectory()
+          )
+          .map(o => path.join(basePath, 'package/option', o))
+      : []),
   ],
 }
